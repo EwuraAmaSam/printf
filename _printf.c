ae2120bd3bd67c5e5fb337117521eeca8f507ed4
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-03-28 17:29:48 -0700

    Updated _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,49 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * _printf - prints texts to the console
- * @format: parameter to be printed
- * Return: number of chars printed
- */
 
+/**
+ * _printf - prints output according to a format
+ * @format: character string containing zero or more directives
+ * Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
-            else if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(args, char *);
-                while (*s)
-                {
-                    putchar(*s);
-                    s++;
-                    count++;
-                }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return count;
-}
+	int count = 0;
+	va_list args;
 
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+
+				fputs(s, stdout);
+				count += strlen(s);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
